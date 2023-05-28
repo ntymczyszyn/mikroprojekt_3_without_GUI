817c94286bc631167dc463c272191caa52468a8e
Natalia Tymczyszyn <116519132+ntymczyszyn@users.noreply.github.com>
2023-05-28 17:47:37 +0200

    Update Player.h

--- include/Player.h
+++ include/Player.h
@@ -1,12 +1,13 @@
 #ifndef MIKROPROJEKT_3_WITHOUT_GUI_PLAYER_H
 #define MIKROPROJEKT_3_WITHOUT_GUI_PLAYER_H
 #include <vector>
+#include <string>
 
 class Player{
 public:
     Player(char type_, int point);
-     virtual std::vector<int> makeMove() = 0; // do I need virtual keyword
-    //bool validateMove();
+    virtual std::vector<int> makeMove() = 0; // do I need virtual keyword?
+    bool isValid(std::string choice);
     char getType() const;
     int getPoint() const;
 private:
