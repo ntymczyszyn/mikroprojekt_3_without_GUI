34d2a4c5569bda313e9672f097e7106430ab4837
Natalia Tymczyszyn <116519132+ntymczyszyn@users.noreply.github.com>
2023-05-28 17:49:07 +0200

    Update HumanPlayer.h

--- include/HumanPlayer.h
+++ include/HumanPlayer.h
@@ -7,8 +7,7 @@ class HumanPlayer: public Player{
 public:
     HumanPlayer(char type_, int point);
     std::vector<int> makeMove()  override;
-    //bool validateMove();
-
+    bool isValid(std::string choice);
 };
 
 #endif //MIKROPROJEKT_3_WITHOUT_GUI_HUMANPLAYER_H
